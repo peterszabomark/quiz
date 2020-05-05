ff1b6b0752b6b863a4e08d123d42aa7ca8c5bb39
Peter-Szabo Mark <peterszabomark@gmail.com>
2020-05-05 18:29:50 +0200

    @ui-1: deleted original page, added question-card component and answer-card component

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionCardComponent } from './question-card/question-card.component';
+import { AnswerCardComponent } from './answer-card/answer-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionCardComponent,
+    AnswerCardComponent
   ],
   imports: [
     BrowserModule,
