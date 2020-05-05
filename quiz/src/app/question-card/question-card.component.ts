098dd7b56474eb59fd1443184281f54d93e2308e
Peter-Szabo Mark <peterszabomark@gmail.com>
2020-05-05 22:46:43 +0200

    @ui-4: added filter to separate a category

--- quiz/src/app/question-card/question-card.component.ts
+++ quiz/src/app/question-card/question-card.component.ts
@@ -12,6 +12,9 @@ export class QuestionCardComponent implements OnInit {
 
   question:any;
   answer:any;
+  separateQuestions: any;
+  categoryId = 0;
+  itemIndex: any;
   categoryd: Category[] = [
     {id:0, name:"Random"},
     {id:21, name:"Animals"},
@@ -21,6 +24,7 @@ export class QuestionCardComponent implements OnInit {
     {id:17, name:"U.S. States"},
     {id:8, name:"Time"},
   ]
+  defaultCategory = 0;
 
   constructor(private http:HttpClient) { }
 
@@ -34,12 +38,41 @@ export class QuestionCardComponent implements OnInit {
   }
 
   addNewQuestion() {
-    let response = this.http.get("http://jservice.io/api/random");
+    if(this.categoryId == 0){
+      let response = this.http.get("http://jservice.io/api/random");
       response.subscribe((data) => {
         this.question = data[0].question;
         this.answer= data[0].answer
       }
       )
+    }else{
+      if(this.itemIndex < this.separateQuestions.length){
+        this.question = this.separateQuestions[this.itemIndex].question;
+        this.answer = this.separateQuestions[this.itemIndex].answer;
+        this.itemIndex++
+      } else {
+        console.log("Ready with question")
+        this.itemIndex = 0;
+      }
+    
+      
+    }
+  }
+
+  filterCategory(value) {
+    if(value!=0){
+      let practice = this.http.get("http://jservice.io/api/category?id=" + value );
+      practice.subscribe((data: any) => {
+        this.separateQuestions = data.clues
+        this.categoryId = value ;
+        console.log(this.separateQuestions)
+        this.itemIndex = 0;
+        this.addNewQuestion();
+      });
+    } else {
+      this.categoryId = value;
+      this.addNewQuestion();
+    }
   }
 
 }
